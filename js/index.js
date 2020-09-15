8257ee1488925c4107276d12652e5e2256fb069e
aline-martins <aline.malonso@gmail.com>
2020-09-16 09:56:28 +1000

    Update date picker

--- js/index.js
+++ js/index.js
@@ -7,6 +7,7 @@
     const inputDescription = document.querySelector('#descriptionFormControlTextArea');
     const inputAssignTo = document.querySelector('#emailFormControlInput');
     const formControls = form.querySelectorAll('.form-control');
+    const inputDueDate = document.querySelector("#duedate");
 
     form.addEventListener('submit', (event) => {
         validateTaskForm();
@@ -19,7 +20,7 @@
         });
     });
 
-    
+
     //Functions
 
     function validateTaskForm() {
@@ -45,13 +46,27 @@
             inputAssignTo.classList.add('is-valid');
         }
 
+        // const currentDate = new Date();
+        // const inputdate = inputDueDate.value;  /** in the form of 2020-9-11 */
+        // let partOfInputDate = inputdate.split("-");
+        // if (Number(partOfInputDate[0]) > currentDate.getUTCFullYear) {
+        //     console.log("Future Year");
+        // }
+        // //console.log(part[2]);
+        // if (Number(partOfInputDate[2]) >= currentDate.getUTCDate()) {
+        //     console.log("valid input date");
+        // }
+        // else {
+        //     console.log("Enter current or future date");
+        // }
+
     }
 
     function clearValidation(control) {
         control.classList.remove('is-invalid');
         control.classList.remove('is-valid');
     }
-    
+
 })();
 
 
