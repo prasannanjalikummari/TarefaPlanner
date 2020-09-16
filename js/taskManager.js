d7c39df096ebfdedc7e92b5022b31437e9fbfec4
aline-martins <aline.malonso@gmail.com>
2020-09-16 17:27:35 +1000

    Add Task HTML with JS

--- js/taskManager.js
+++ js/taskManager.js
@@ -1,3 +1,33 @@
+const createTaskHtml = (name, description, assignedTo, dueDate, status) => 
+    `
+        <div class="col-md-12 col-lg-4">
+            <div class="card">
+                <div class="text-center border-info">
+                    <div class="card-header">
+                        ${status}
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title display-4">${name}</h5>
+                        <p class="card-text mt-2">${description}</p>
+                        <p class="card-text mt-5">
+                            <i class="fa fa-calendar" aria-hidden="true"></i>
+                            ${dueDate}
+                        </p>
+                        <a href="#" class="btn btn-primary">
+                            <i class="fa fa-file-text-o" aria-hidden="true"></i>
+                        </a>
+                        <a href="#" class="btn btn-danger">
+                            <i class="fa fa-trash-o" aria-hidden="true"></i>
+                        </a>
+                    </div>
+                    <div class="card-footer text-muted">
+                        ${assignedTo}
+                    </div>
+                </div>
+            </div>
+        </div>
+   `;
+
 class taskManager {
     constructor(currentId = 0) {
         this.tasks = [];
@@ -16,4 +46,13 @@ class taskManager {
 
         this.tasks.push(task);
     }
+
+     
+    render() {
+        const taskHtmlList = [];
+        for(i = 0; i < this.task.length; i++) {
+          
+        }
+    }
+
 }
