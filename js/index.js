954d894a923b9ba4663a2866cb6d975f36ca56f1
Prasannanjali <prasannanjali.kummari@gmail.com>
2020-09-18 13:49:56 +1000

    Update the selected task status to DONE

--- js/index.js
+++ js/index.js
@@ -1,98 +1,95 @@
-const taskM = new TaskManager();   //taskM is an instance of taskManager
+const taskM = new TaskManager(); //taskM is an instance of taskManager
 
 //Form
-const form = document.querySelector('#form');
+const form = document.querySelector("#form");
 
 //Inputs
-const inputName = document.querySelector('#nameFormControlInput');
-const inputDescription = document.querySelector('#descriptionFormControlTextArea');
-const inputAssignTo = document.querySelector('#emailFormControlInput');
+const inputName = document.querySelector("#nameFormControlInput");
+const inputDescription = document.querySelector(
+  "#descriptionFormControlTextArea"
+);
+const inputAssignTo = document.querySelector("#emailFormControlInput");
 const inputDueDate = document.querySelector("#duedate");
 
 //Clear Validation
-const formControls = form.querySelectorAll('.form-control');
+const formControls = form.querySelectorAll(".form-control");
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    validateTaskForm();
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  validateTaskForm();
 
-    const name = inputName.value;
-    const description = inputDescription.value;
-    const assignedTo = inputAssignTo.value;
-    const dueDate = inputDueDate.value;
+  const name = inputName.value;
+  const description = inputDescription.value;
+  const assignedTo = inputAssignTo.value;
+  const dueDate = inputDueDate.value;
 
-    taskM.addTask(name, description, assignedTo, dueDate);
-    taskM.render();
+  taskM.addTask(name, description, assignedTo, dueDate);
+  taskM.render();
 
-    inputName.value = '';
-    inputDescription.value = '';
-    inputAssignTo.value = '';
-    inputDueDate.value = '';
+  inputName.value = "";
+  inputDescription.value = "";
+  inputAssignTo.value = "";
+  inputDueDate.value = "";
 });
 
 formControls.forEach((control) => {
-    control.addEventListener('focus', (event) => {
-        clearValidation(control);
-    });
+  control.addEventListener("focus", (event) => {
+    clearValidation(control);
+  });
 });
-
 
 //Functions
 
 function validateTaskForm() {
+  //Name
+  if (inputName.value.length > 8 || inputName.value === "") {
+    inputName.classList.add("is-invalid");
+  } else {
+    inputName.classList.add("is-valid");
+  }
 
-    //Name
-    if (inputName.value.length > 8 || inputName.value === "") {
-        inputName.classList.add('is-invalid');
-    } else {
-        inputName.classList.add('is-valid');
-    }
+  //Description
+  if (inputDescription.value.length < 15 || inputDescription.value === "") {
+    inputDescription.classList.add("is-invalid");
+  } else {
+    inputDescription.classList.add("is-valid");
+  }
 
-    //Description
-    if (inputDescription.value.length < 15 || inputDescription.value === "") {
-        inputDescription.classList.add('is-invalid');
-    } else {
-        inputDescription.classList.add('is-valid');
-    }
+  //AssignedTo
+  if (inputAssignTo.value.length < 8 || inputAssignTo.value === "") {
+    inputAssignTo.classList.add("is-invalid");
+  } else {
+    inputAssignTo.classList.add("is-valid");
+  }
 
-    //AssignedTo
-    if (inputAssignTo.value.length < 8 || inputAssignTo.value === "") {
-        inputAssignTo.classList.add('is-invalid');
-    } else {
-        inputAssignTo.classList.add('is-valid');
-    }
-
-    //Date
-    // const currentDate = new Date();
-    // const inputDate = inputDueDate.value;  /** in the form of 2020-9-11 */
-    // let partOfInputDate = inputDate.split("-");
-    // if (Number(partOfInputDate[0]) > currentDate.getUTCFullYear) {
-    //     console.log("Future Year");
-    // }
-    // //console.log(part[2]);
-    // if (Number(partOfInputDate[2]) >= currentDate.getUTCDate()) {
-    //     console.log("valid input date");
-    // }
-    // else {
-    //     console.log("Enter current or future date");
-    // }
-
+  //Date
+  // const currentDate = new Date();
+  // const inputDate = inputDueDate.value;  /** in the form of 2020-9-11 */
+  // let partOfInputDate = inputDate.split("-");
+  // if (Number(partOfInputDate[0]) > currentDate.getUTCFullYear) {
+  //     console.log("Future Year");
+  // }
+  // //console.log(part[2]);
+  // if (Number(partOfInputDate[2]) >= currentDate.getUTCDate()) {
+  //     console.log("valid input date");
+  // }
+  // else {
+  //     console.log("Enter current or future date");
+  // }
 }
 
 function clearValidation(control) {
-    control.classList.remove('is-invalid');
-    control.classList.remove('is-valid');
+  control.classList.remove("is-invalid");
+  control.classList.remove("is-valid");
 }
 
-const taskList = document.querySelector('#tasksList');
-// taskList.addEventListener('click',(event) => {
-// if (event.target.classList.contains('done-button')){
-
-// }
-// });
-
-
-
-
-
-
+const taskList = document.querySelector("#tasksList");
+taskList.addEventListener("click", (event) => {
+  if (event.target.classList.contains("done-button")) {
+    const parentTask = event.target.parentElement.parentElement.parentElement;  
+    const taskId = Number(parentTask.dataset.taskId);
+    const task = taskM.getTaskById(taskId);
+    task.status = 'DONE';
+    taskM.render();
+  }
+});
--- js/taskManager.js
+++ js/taskManager.js
@@ -1,7 +1,7 @@
-const createTaskHtml = (name, description, assignedTo, dueDate, status) => {
-    return `
+const createTaskHtml = (name, description, assignedTo, dueDate, status, id) => {
+  return `
         <div class="col-md-12 col-lg-4">
-            <div class="card">
+            <div class="card" data-task-id=${id}>
                 <div class="text-center border-info">
                     <div class="card-header">
                         ${status}
@@ -33,44 +33,79 @@ const createTaskHtml = (name, description, assignedTo, dueDate, status) => {
 };
 
 class TaskManager {
-    constructor(currentId = 0) {
-        this.tasks = [];
-        this.currentId = currentId;
+  constructor(currentId = 0) {
+    this.tasks = [];
+    this.currentId = currentId;
+  }
+
+  addTask(name, description, assignedTo, dueDate) {
+    const task = {
+      id: this.currentId++,
+      name: name,
+      description: description,
+      assignedTo: assignedTo,
+      dueDate: dueDate,
+      status: "TODO",
+    };
+
+    this.tasks.push(task);
+  }
+
+  render() {
+    const taskHtmlList = [];
+
+    for (let i = 0; i < this.tasks.length; i++) {
+      const task = this.tasks[i];
+      const date = new Date(task.dueDate);
+      const formattedDate = `${date.getDate()}/${
+        date.getMonth() + 1
+      }/${date.getFullYear()}`;
+      const taskHTML = createTaskHtml(
+        task.name,
+        task.description,
+        task.assignedTo,
+        formattedDate,
+        task.status,
+        task.id
+      );
+
+      taskHtmlList.push(taskHTML);
     }
 
-    addTask(name, description, assignedTo, dueDate) {
-        const task = {
-            id: this.currentId++,
-            name: name,
-            description: description,
-            assignedTo: assignedTo,
-            dueDate: dueDate,
-            status: "TODO",
-        };
-
-        this.tasks.push(task);
-    }
-
-    render() {
-        const taskHtmlList = [];
-
-        for (let i = 0; i < this.tasks.length; i++) {
-            const task = this.tasks[i];
-            const date = new Date(task.dueDate);
-            const formattedDate = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
-            const taskHTML = createTaskHtml(
-                task.name,
-                task.description,
-                task.assignedTo,
-                formattedDate,
-                task.status
-            );
-
-            taskHtmlList.push(taskHTML);
+    const tasksHtml = taskHtmlList.join("\n");
+    const card = document.querySelector("#tasksList");
+    card.innerHTML = tasksHtml;
+  }
+  getTaskById(taskId) {
+    let foundTask;
+    for (let i = 0; i < this.tasks.length; i++) {
+        const task = this.tasks[i];
+        if (task.id === taskId)
+        {
+            foundTask = task;
         }
-
-        const tasksHtml = taskHtmlList.join("\n");
-        const card = document.querySelector("#tasksList");
-        card.innerHTML = tasksHtml;
     }
+    return foundTask;
+  }
+
+/*changeColourWithStatus()
+{
+    for (let i=0; i< this.tasks.length; i++)
+    { let changeColourValue;
+    const task = this.tasks[i];
+    let changeColour = task;
+    if (task.status === 'TODO')
+    {   
+    changeColourValue =0;
+   // console.log(changeColour);
+   }
+   if (task.status === 'DONE')
+   {
+    changeColourValue = 1;
+   }else{
+       changeColourValue = 2;
+   }
+    }
+    return (changeColour,changeColourValue);
+}*/
 }
